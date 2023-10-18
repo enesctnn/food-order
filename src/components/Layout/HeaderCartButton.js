08448336485acf6b2e530754853833753548f704
enesctnn <enespcetin@gmail.com>
2023-10-18 16:27:00 +0300

    header cart button effect when item added

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,34 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
 import CartContext from '../../store/cart-context';
 
 const HeaderCartButton = (props) => {
+  const [buttonIsHighlighted, setButtonIsHighlighted] = useState(false);
   const { items } = useContext(CartContext);
   const numberOfCartItems = items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
+
+  const btnClasses = `${classes.button} ${
+    buttonIsHighlighted ? classes.bump : ''
+  }`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setButtonIsHighlighted(true);
+    const timeId = setTimeout(() => {
+      setButtonIsHighlighted(false);
+    }, 300);
+    return () => {
+      clearTimeout(timeId);
+    };
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={props.onCartClick}>
+    <button className={btnClasses} onClick={props.onCartClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
